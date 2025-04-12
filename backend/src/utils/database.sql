ef30b3065aa9622c44dab89071aad6beed637313
Mathieu <hei.tafita.2@gmail.com>
2025-04-12 08:10:28 +0300

    feat: update database.sql by adding google_id

--- backend/src/utils/database.sql
+++ backend/src/utils/database.sql
@@ -8,6 +8,7 @@ CREATE TABLE users (
   id SERIAL PRIMARY KEY,
   email VARCHAR(255) UNIQUE NOT NULL,
   password VARCHAR(255) NOT NULL,
+  google_id VARCHAR(255) UNIQUE,
   created_at TIMESTAMPTZ DEFAULT NOW(),
   updated_at TIMESTAMPTZ DEFAULT NOW(),
   CONSTRAINT valid_email CHECK (email ~* '^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+[.][A-Za-z]+$')
