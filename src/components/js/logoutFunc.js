e802c1d51e8435d718539b4109f9a578594d4197
Jonathan <hei.jonathan.3@gmail.com>
2025-04-19 14:43:39 +0300

    fix: make the logout button work

--- src/components/js/logoutFunc.js
+++ src/components/js/logoutFunc.js
@@ -1,5 +1,9 @@
+const logoutBtn = document.getElementById("logout-btn");
+
 const logout = () => {
-  localStorage.removeItem('typing_game_user');
-  localStorage.removeItem('typing_game_token');
-  window.location.replace('/index.html');
+  localStorage.removeItem("typing_game_user");
+  localStorage.removeItem("typing_game_token");
+  window.location.replace("/index.html");
 };
+
+logoutBtn.addEventListener("click", logout);
